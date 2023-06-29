7845d7f8eb91230b10979acb10b75487ccb61cc6
Omakku <119434173+Omakku@users.noreply.github.com>
2023-06-28 20:43:47 -0400

    Add files via upload

--- login.js
+++ login.js
@@ -1,27 +1,51 @@
 const mysql = require('mysql');
+const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv');
+const cookieParser = require('cookie-parser');
+
 
 dotenv.config({ path: './.env'})
 
+//------ Initialize server
 const app = express();
 
-app.use("/public", express.static(__dirname + "/public"));
-
+//------ Initialize database connection
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "brazil20!",
-    database: "mydb"
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE
 });
 
+//----- Get css and image files from public dir
+app.use(express.static(__dirname + '/public'));
+
+//----- Set view engine and location of views folder
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'ejs');
+
+
+//-----Test Connection to SQL Database
 db.connect( (error) => {
     if(error){
         console.log(error)
     }
     else{
-        console.log("Successful Connection")
+        console.log("Successful Database Connection")
     }
 })
 
-app.listen(4500); 
+
+//----- Get routes
+app.use('/', require('./routes/pages'));
+app.use('/auth',require('./routes/auth'));
+
+//------------------------------------------
+app.use(express.urlencoded({ extended: false}));
+app.use(express.json());
+app.use(cookieParser()); 
+
+app.listen(4500, () => {
+    console.log("Server is running on port 4500");
+})
